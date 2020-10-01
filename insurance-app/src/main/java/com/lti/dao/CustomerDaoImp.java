ffc21d1f0463f344d582b7ea2d4f2be21f47c38f
Anubhav147 <anubhavraj149@gmail.com>
2020-10-01 11:38:27 +0530

    Reg

--- insurance-app/src/main/java/com/lti/dao/CustomerDaoImp.java
+++ insurance-app/src/main/java/com/lti/dao/CustomerDaoImp.java
@@ -1,5 +1,24 @@
 package com.lti.dao;
 
-public class CustomerDaoImp {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lti.entity.Customer;
+
+@Repository
+public class CustomerDaoImp implements CustomerDao{
+
+	@PersistenceContext
+	EntityManager entityManager;
+	
+	@Override
+	@Transactional
+	public Customer register(Customer customer){
+			return entityManager.merge(customer);
+
+		}
+		
 }

b06f86ce1d85c4210d28438753b213cfc5cb67c1
rishabh208gupta <rishabhgupta.52pp@gmail.com>
2020-10-01 18:05:33 +0530

    added date to buy controller

--- insurance-app/src/main/java/com/lti/controller/BuyPolicyController.java
+++ insurance-app/src/main/java/com/lti/controller/BuyPolicyController.java
@@ -1,5 +1,7 @@
 package com.lti.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,6 +64,7 @@ public class BuyPolicyController {
 	@PostMapping("/make-payment")
 	public PaymentStatus makePayment(@RequestBody Payment payment) {
 		try {
+			payment.setPaymentDate(LocalDate.now());
 			payment = buyPolicyService.makePayment(payment);
 			PaymentStatus paymentStatus = new PaymentStatus();
 			paymentStatus.setStatus(true);

9086be36f9511373a0bd876de822f38eb95250ae
Anubhav147 <anubhavraj149@gmail.com>
2020-10-01 13:21:27 +0530

    Registration Complete

--- insurance-app/src/main/java/com/lti/controller/RegisterController.java
+++ insurance-app/src/main/java/com/lti/controller/RegisterController.java
@@ -1,5 +1,45 @@
 package com.lti.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lti.dto.Registration;
+import com.lti.dto.Status;
+import com.lti.entity.Customer;
+import com.lti.exception.RegisterServiceException;
+import com.lti.service.RegisterService;
+
+@RestController
+@CrossOrigin
 public class RegisterController {
+	
+	@Autowired
+	private RegisterService registerService;
+	
+	@PostMapping(path = "/register")
+	// similar to @RequestMapping(path = "/register", method=RequestMethod.POST)
+	public Status registerCustomer(@RequestBody Registration registration) {
+		Customer customer = new Customer(); 
+		BeanUtils.copyProperties(registration, customer);
+		try {
+			int id = registerService.registeration(customer);
+
+			Status status = new Status();
+		//	status.setCustomerId(id);
+			status.setStatus(true);
+			status.setStatusMessage("Registration Successful");
+			return status;
+		} catch (RegisterServiceException e) {
+			Status status = new Status();
+			status.setStatus(false);
+			status.setStatusMessage(e.getMessage());
+			return status;
+		}
+
+	}
 
 }
--- insurance-app/src/main/java/com/lti/service/RegisterService.java
+++ insurance-app/src/main/java/com/lti/service/RegisterService.java
@@ -1,11 +1,13 @@
 package com.lti.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.lti.dao.CustomerDao;
 import com.lti.entity.Customer;
 import com.lti.exception.RegisterServiceException;
 
+@Service
 public class RegisterService {
 	
 	@Autowired

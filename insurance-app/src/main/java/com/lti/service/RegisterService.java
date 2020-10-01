19e7baa556916b5ad56bb7845985a9a73d056079
Anubhav147 <anubhavraj149@gmail.com>
2020-10-01 11:51:01 +0530

    reg

--- insurance-app/src/main/java/com/lti/service/RegisterService.java
+++ insurance-app/src/main/java/com/lti/service/RegisterService.java
@@ -2,16 +2,6 @@ package com.lti.service;
 
 public class RegisterService {
 	
-	@Override
-	public int register(Customer customer) {
-		if (!customerRepository.isCustomerPresent(customer.getEmail())) {
-			int id = customerRepository.save(customer);
-			emailService.sendMailForNewRegistration(customer);
-			return id;
-//			then code to send an email to the customer will be here !!
-		} else {
-			throw new CustomerServiceException("Customer Already Registered");
-		}
-	}
+	
 
 }

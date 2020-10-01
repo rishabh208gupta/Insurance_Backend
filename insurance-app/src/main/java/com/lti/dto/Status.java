22e5fc42e46fe79e6527205ef88b0070650d8031
rishabh208gupta <rishabhgupta.52pp@gmail.com>
2020-10-01 11:11:50 +0530

    updated Status class in dto package

--- insurance-app/src/main/java/com/lti/dto/Status.java
+++ insurance-app/src/main/java/com/lti/dto/Status.java
@@ -2,4 +2,20 @@ package com.lti.dto;
 
 public class Status {
 
+	private boolean status;
+	private String statusMessage;
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public String getStatusMessage() {
+		return statusMessage;
+	}
+	public void setStatusMessage(String statusMessage) {
+		this.statusMessage = statusMessage;
+	}
+	
+	
 }

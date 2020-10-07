3b4df1a9b7c9b0139d50ef7710bb62dc68c23a74
shivani <shivanijsh97@gmail.com>
2020-10-07 11:54:43 +0530

    updated calculate premium formula

--- insurance-app/src/main/java/com/lti/service/CalculatePremiumService.java
+++ insurance-app/src/main/java/com/lti/service/CalculatePremiumService.java
@@ -24,8 +24,22 @@ public class CalculatePremiumService {
 		depreciatonRate=11;
 		age=LocalDate.now().getYear()-purchaseDate.getYear();
 		
-		idv=vehiclePrice-(vehiclePrice*age*depreciatonRate)/100; 
-		estimatedValue=(idv*premiumRate*planYear)/100;
+		if(age>7) {
+			if(vehiclePrice==50000) 
+				idv=5000;
+			else
+				idv=25000;
+				
+		}
+		else {
+			idv=vehiclePrice-(vehiclePrice*age*depreciatonRate)/100; 
+		}
+		
+		if(vehiclePrice==50000) 
+			estimatedValue=(idv*premiumRate*planYear)/100;
+		else
+			estimatedValue=(idv*premiumRate*0.5*planYear)/100;
+		
 		
 		PremiumAnswer premiumAnswer= new PremiumAnswer();
 		premiumAnswer.setEstimatedValue(estimatedValue);
